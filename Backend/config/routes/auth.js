33d99a21a9d48c5faee9a68c37dd7cb987b77ff2
Saim Amir <pubglatest0@gmail.com>
2025-03-29 23:32:28 +0500

    Create auth.js

---
+++ Backend/config/routes/auth.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const authController = require('../controllers/auth');
+
+// @route   POST api/auth/register
+// @desc    Register user
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be 6+ characters').isLength({ min: 6 })
+  ],
+  authController.register
+);
+
+// @route   POST api/auth/login
+// @desc    Login user
+router.post(
+  '/login',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  authController.login
+);
+
+module.exports = router;
