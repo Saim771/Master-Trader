6cd4c67ef30454ee9a58fb22e3051ee76d1c86c9
Saim Amir <pubglatest0@gmail.com>
2025-03-29 23:40:48 +0500

    Create auth.js

---
+++ Backend/config/Middlewares/auth.js
@@ -0,0 +1,51 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Protect routes with JWT authentication
+exports.protect = async (req, res, next) => {
+  let token;
+
+  // Get token from header
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // Get token from cookie
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      error: 'Not authorized to access this route'
+    });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    // Get user from database
+    req.user = await User.findById(decoded.user.id).select('-password');
+    
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      error: 'Not authorized to access this route'
+    });
+  }
+};
+
+// Role-based authorization (Admin, User, etc.)
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        error: `User role ${req.user.role} is not authorized to access this route`
+      });
+    }
+    next();
+  };
+};
